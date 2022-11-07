06b6f518f02ed41f1a5382ae2d9229cab6e7329c
Alex Domingo <alex.domingo.toro@vub.be>
2022-11-07 15:27:47 +0100

    JupyterHub modules have no use for users

--- SitePackage.lua
+++ SitePackage.lua
@@ -234,6 +234,8 @@ local function visible_hook(modT)
         modT.isVisible = false
     elseif modT.fullName:find("EESSI/") then
         modT.isVisible = false
+    elseif modT.fullName:find("JupyterHub/") then
+        modT.isVisible = false
     elseif module_age(modT) > 5 then
         modT.isVisible = false
     end
